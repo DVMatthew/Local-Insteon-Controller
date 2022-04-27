9c2d81e5ef1848e0f5b2d18750b682b34d54dfe3
Matthew <42288388+DVMatthew@users.noreply.github.com>
2022-04-27 12:20:22 -0400

    Scheduling Done

--- public/js/timer.js
+++ public/js/timer.js
@@ -1,7 +1,3 @@
-document.getElementById("onForm").addEventListener("click", (e) => {
-    e.preventDefault();
-});
-
 function deviceOn(device) {
     $.post("http://192.168.1.26:8080/on", {
         deviceID: `${device}` 
@@ -19,20 +15,43 @@ function deviceOff(device) {
     })
 }
 
-function scheduleDevice(timeInMills, off, device) {
-    var now = new Date();
-    if(off == true) {
-        if(now.getTime() == timeInMills) {
+function scheduleDevice(time, off, device, complete) {
+    var hour = new Date().getHours();
+    var minute = new Date().getMinutes();
+    var second = new Date().getSeconds();
+
+    if(time == `${hour}` + `${minute}` + `${second}`) {
+        if(off == true) {
             deviceOff(device);
-        }
-    } else if(off != true) {
-        if(now.getTime() == timeInMills) {
+        } 
+        if(off == false) {
             deviceOn(device);
         }
+    } else if(time != `${hour}` + `${minute}` + `${second}`) {
+        return;
     }
+
 }
 
 setInterval(() => {
-    scheduleDevice(39600000, false, "5617CC");
+    document.getElementById("onForm").addEventListener("click", (e) => {
+        e.preventDefault();
+    });
+    
+    // Spotlight
+    scheduleDevice(19001, false, "5617CC");
+    scheduleDevice(23001, true, "5617CC");
 
-}, 1);
+    // Landscape Lights
+    scheduleDevice(19002, false, "5618B3");
+    scheduleDevice(23002, true, "5618B3");
+
+    // Porch Light
+    scheduleDevice(19003, false, "43A5CD");
+    scheduleDevice(7003, true, "43A5CD");
+
+    // Shed Outdoor Lights
+    scheduleDevice(19004, false, "5103CF");
+    scheduleDevice(23004, true, "5103CF");
+
+}, 700);
